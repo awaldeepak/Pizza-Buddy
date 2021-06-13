88f5fae3fabe1513995aa8a6883d982c58b07009
Deepak Awal <awaldeepak.1997@gmail.com>
2021-06-13 12:23:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+
+
+//Display home view at root route
+app.get('/', (req, res) => {
+
+    return res.render('home', {
+        title: 'Home'
+    });
+
+});
+
+
+//Set Template Engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+//Listen Server on Port 8000
+app.listen(PORT, (err) => {
+    if(err){ console.log(`Error: ${err}`); }
+
+    console.log(`Server is up and running on port ${PORT}`);
+});
